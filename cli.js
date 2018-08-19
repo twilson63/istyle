822e85589e867ab71f2e6a194140fb4a5ebc28f3
Tom Wilson <twilson@TRSCLP104.local>
2018-08-18 20:13:10 -0400

    added six styles

--- cli.js
+++ cli.js
@@ -4,17 +4,30 @@ var fs = require('fs')
 var style = process.argv[2] 
 
 if (!style) {
-  console.log('**** istyle ****')
-  console.log('*              *')
-  console.log('* Gen stylesheet *')
-  console.log('* ============== *')
-  console.log('* github         *')
-  console.log('* grump          *')
-  console.log('******************')
+  console.log('**** istyle *******')
+  console.log('*                 *')
+  console.log('* Gen stylesheet  *')
+  console.log('* ==============  *')
+  console.log('* avenue          *')
+  console.log('* amblin          *')
+  console.log('* antique         *')
+  console.log('* manuscript      *')
+  console.log('* swiss           *')
+  console.log('* github          *')
+  console.log('* grump           *')
+  console.log('* kult            *')
+  console.log('*******************')
   console.log('* istyle github > style.css *')
   return
 
 }
+
+const styles = ['avenue', 'amblin', 'antique', 'manuscript', 'swiss', 'github', 'grump','kult']
+
+if (styles.indexOf(style) < 0) { 
+  console.log('No Match Found!')
+  return 
+} 
 
 process.stdout.write(fs.readFileSync(__dirname + '/' + style + '.css', 'utf-8'))
 
